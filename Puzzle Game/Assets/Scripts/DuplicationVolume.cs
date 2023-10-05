5f062479fcd98b5229a8d3f925422900dc7e0e42
William Pittenger <106987854+wbp1702@users.noreply.github.com>
2023-10-05 12:14:19 -0500

    End of Class
    - Fixed DuplicationVolume not catching objects.
    - Sorted Asset Files

--- Puzzle Game/Assets/Scripts/DuplicationVolume.cs
+++ Puzzle Game/Assets/Scripts/DuplicationVolume.cs
@@ -7,12 +7,13 @@ public class DuplicationVolume : MonoBehaviour
 {
 	[Tooltip("The global scale of the volume, used to scale duplicated objects to avoid use of lossyScale.")]
 	public float globalScale = 1.0f;
-	
+
+	[Tooltip("The duplication volume that this volume will duplicate to and vise versa.")]
 	public DuplicationVolume twin;
 	
 	private Dictionary<GameObject, GameObject> duplicates = new();
 
-	void LateUpdate()
+	void Update()
     {
 		List<GameObject> toRemove = new();
 
@@ -44,23 +45,8 @@ public class DuplicationVolume : MonoBehaviour
 
 	private void OnTriggerEnter(Collider other)
 	{
-		if (other.gameObject.layer != LayerMask.NameToLayer("Duplicatable")) return;
-
-		var relativePosition = transform.InverseTransformPoint(other.transform.position);
-		var relativeRotation = Quaternion.FromToRotation(transform.forward, twin.transform.forward);
-
-		var duplicate = Instantiate(other.gameObject);
-		duplicate.layer = LayerMask.NameToLayer("TransparentFX");
-		duplicate.transform.position = twin.transform.TransformPoint(relativePosition);
-		duplicate.transform.rotation = relativeRotation * other.transform.rotation;
-
-        duplicate.transform.localScale = Vector3.one;
-		duplicate.transform.localScale = new Vector3(other.transform.lossyScale.x / duplicate.transform.lossyScale.x * globalScale / twin.globalScale,
-													 other.transform.lossyScale.y / duplicate.transform.lossyScale.y * globalScale / twin.globalScale,
-													 other.transform.lossyScale.z / duplicate.transform.lossyScale.z * globalScale / twin.globalScale);
-
-		duplicates.Add(other.gameObject, duplicate);
-	}
+		if (other.gameObject.layer == LayerMask.NameToLayer("Duplicatable") && !duplicates.ContainsKey(other.gameObject)) AddDuplicate(other.gameObject);
+    }
 
 	private void OnTriggerExit(Collider other)
 	{
@@ -68,4 +54,22 @@ public class DuplicationVolume : MonoBehaviour
 
 		if (duplicates.Remove(other.gameObject, out GameObject duplicate)) Destroy(duplicate);
 	}
+
+	public void AddDuplicate(GameObject other)
+	{
+        var relativePosition = transform.InverseTransformPoint(other.transform.position);
+        var relativeRotation = Quaternion.FromToRotation(transform.forward, twin.transform.forward);
+
+        var duplicate = Instantiate(other.gameObject);
+        duplicate.layer = LayerMask.NameToLayer("TransparentFX");
+        duplicate.transform.position = twin.transform.TransformPoint(relativePosition);
+        duplicate.transform.rotation = relativeRotation * other.transform.rotation;
+
+        duplicate.transform.localScale = Vector3.one;
+        duplicate.transform.localScale = new Vector3(other.transform.lossyScale.x / duplicate.transform.lossyScale.x * globalScale / twin.globalScale,
+                                                     other.transform.lossyScale.y / duplicate.transform.lossyScale.y * globalScale / twin.globalScale,
+                                                     other.transform.lossyScale.z / duplicate.transform.lossyScale.z * globalScale / twin.globalScale);
+
+        duplicates.Add(other.gameObject, duplicate);
+    }
 }
--- Puzzle Game/Assets/Scripts/Teleport Volume.cs
+++ Puzzle Game/Assets/Scripts/Teleport Volume.cs
@@ -10,10 +10,11 @@ public class TeleportVolume : MonoBehaviour
     
     [Tooltip("The position the player will be teleported relative to based of their relative position to the center of the volume.")]
     public GameObject relativeDestination;
+
+    [Tooltip("If the destination is inside a duplication volume then this field forces the teleported object to be duplicated this frame.")]
+    public DuplicationVolume duplicationVolume;
     
     private Collider[] colliders;
-
-    private List<GameObject> targets = new();
 
     // Start is called before the first frame update
     void Start()
@@ -25,43 +26,35 @@ public class TeleportVolume : MonoBehaviour
 
     private void OnTriggerStay(Collider other) => ShouldTeleport(other);
 
-    private void Update()
-    {
-        foreach (var other in targets)
-        {
-            Rigidbody rigidbody = other.GetComponent<Rigidbody>();
-
-            var relativePosition = transform.InverseTransformPoint(other.transform.position);
-            Quaternion relativeRotation = Quaternion.FromToRotation(transform.forward, relativeDestination.transform.forward);
-
-            var desiredLossy = globalScale * other.transform.lossyScale;
-
-            other.transform.localScale = Vector3.one;
-            var otherScale = other.transform.lossyScale;
-
-            other.transform.localScale = new Vector3(otherScale.x * desiredLossy.x, otherScale.y * desiredLossy.y, otherScale.z * desiredLossy.z);
-
-            var velocity = rigidbody.velocity * globalScale;
-            var angularVelocity = rigidbody.angularVelocity;
-
-            rigidbody.isKinematic = true;
-            other.transform.position = relativeDestination.transform.TransformPoint(relativePosition);
-            other.transform.rotation = relativeRotation * other.transform.rotation;
-            rigidbody.isKinematic = false;
-            rigidbody.velocity = relativeRotation * velocity;
-            rigidbody.angularVelocity = relativeRotation * angularVelocity;
-            Debug.Break();
-        }
-        targets.Clear();
-    }
-
     private void ShouldTeleport(Collider other)
     {
         foreach (var collider in colliders)
         {
             if (collider.bounds.Contains(other.gameObject.transform.position))
             {
-                targets.Add(other.gameObject);
+                Rigidbody rigidbody = other.GetComponent<Rigidbody>();
+
+                var relativePosition = transform.InverseTransformPoint(other.transform.position);
+                Quaternion relativeRotation = Quaternion.FromToRotation(transform.forward, relativeDestination.transform.forward);
+
+                var desiredLossy = globalScale * other.transform.lossyScale;
+
+                other.transform.localScale = Vector3.one;
+                var otherScale = other.transform.lossyScale;
+
+                other.transform.localScale = new Vector3(otherScale.x * desiredLossy.x, otherScale.y * desiredLossy.y, otherScale.z * desiredLossy.z);
+
+                var velocity = rigidbody.velocity * globalScale;
+                var angularVelocity = rigidbody.angularVelocity;
+
+                rigidbody.isKinematic = true;
+                other.transform.position = relativeDestination.transform.TransformPoint(relativePosition);
+                other.transform.rotation = relativeRotation * other.transform.rotation;
+                rigidbody.isKinematic = false;
+                rigidbody.velocity = relativeRotation * velocity;
+                rigidbody.angularVelocity = relativeRotation * angularVelocity;
+
+                if (duplicationVolume) duplicationVolume.AddDuplicate(other.gameObject);
                 //Debug.Break();
                 return;
             }
